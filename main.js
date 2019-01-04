933a84978f9a9d6aeef4f87dcb4fa4f92b43c95a
Josie Garza <jpg7@rice.edu>
2019-01-04 11:50:38 -0500

    main added

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 // This code is responsible for the execution of other code
-const utils = require('./utils');
-console.log('hello world');
+
+const utils = require('./utils');  //bring in utils file
+//const someThing = utils.someThing;
+
+// const { someThing, x, y, findInitials } = utils;  //destructuring
+const { findInitials } = utils;  //destructuring
+// can also do this while requiring
+
+const { readFile } = require('fs'); //gives access to readFile
+
+readFile('./stuff.txt', 'utf8', (err, data) => {
+  if(err) console.log('ERROR', err);
+  else {
+    const dataS = data.split(' ');
+    console.log(Number(dataS[0]) + Number(dataS[1]) + Number(dataS[2]) + Number(dataS[3]));
+    // data is of type string
+  }
+});
+
+// Two Tasks
+// 1. Figure out how to sum all the numbers in the txt file
+// 2. Write a new utils file that has a function called findInitials.
+  // Takes in a string of space separated words and returns initials
+  // of those words 'Josie Garza' --> 'JG'. Create a txt file of
+  // line separated words and get initials
+
+//console.log(someThing);
+
+readFile('./words.txt', 'utf8', (err, data) => {
+  if(err) console.log('ERROR', err);
+  else {
+    console.log(data);
+  }
+});
--- utils.js
+++ utils.js
@@ -1,1 +1,20 @@
-let someThing = 'SOMETHING';
+// let someThing = 'SOMETHING';
+// let x = 5;
+// let y = 10;
+//
+// module.exports = {
+//   someThing: someThing, // can also just say someThing, x, y bc key = value
+//   x: x,
+//   y: y
+// };
+
+const findInitials = (words) => {
+  const arr = words.split(' ');
+  const firstLetters = arr.map(word =>  word[0]);
+  const initials = firstLetters.join('');
+  return initials;
+}
+
+//console.log(findInitials('hello there'));
+
+module.exports = { findInitials };
